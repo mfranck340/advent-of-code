9a6f790e0811e38584a6186a975f6b69d4a86ce0
mfranck340 <franck.yaguargos@edu.uah.es>
2025-12-04 22:12:26 +0100

    add part 2 problem 3

--- 2025/Day3/day_3.py
+++ 2025/Day3/day_3.py
@@ -1,6 +1,6 @@
 # DAY 3
 with open("Day3/input.txt") as f:
-    banks = [line.strip() for line in f]  # quita '\n' y espacios
+	banks = [line.strip() for line in f]
 
 # Part 1
 result = 0
@@ -17,6 +17,30 @@ for bank in banks:
 
 print(result)
 
+# Part 2
+import numpy as np
 
+with open("Day3/input.txt") as f:
+	banks = np.array([[int(c) for c in line.strip()] for line in f])
 
+result = 0
+ini = 0
+digits = 11
+for bank in banks:
+	max_joltage = 0
 
+	for dig in range(12):
+		max = 0
+
+		for i in range(ini, len(bank) - digits):
+			if bank[i] > max:
+				max = bank[i]
+				ini = i + 1
+		digits -= 1
+		max_joltage = (max_joltage * 10) + max
+
+	result += max_joltage
+	digits = 11
+	ini = 0
+
+print(result)
